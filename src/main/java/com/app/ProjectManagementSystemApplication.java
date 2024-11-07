3cfaa69a3c1a7e1e92cdf8636a228b60ce6b8d44
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2024-11-07 17:56:12 +0100

    Configuring database

--- src/main/java/com/app/ProjectManagementSystemApplication.java
+++ src/main/java/com/app/ProjectManagementSystemApplication.java
@@ -2,8 +2,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
+@ComponentScan(basePackages = "com.app.*")
 public class ProjectManagementSystemApplication {
 
 	public static void main(String[] args) {
