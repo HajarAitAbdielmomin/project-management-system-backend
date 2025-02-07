bbfae2dd4a4a0298be18020f1e29e4e7518c6187
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-02-07 16:07:55 +0100

    Add new model and modify relationship between task, backlog, and team member

--- src/main/java/com/app/models/Backlog.java
+++ src/main/java/com/app/models/Backlog.java
@@ -1,56 +1,60 @@
 package com.app.models;
 
+import com.app.enums.TaskStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "projects")
+@Table(name = "backlogs")
 @EntityListeners(AuditingEntityListener.class)
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Project {
-
+public class Backlog {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 
-	@NotBlank(message = "Project title is required")
+	@NotBlank(message = "Task title is required")
 	@Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
 	@Column(nullable = false)
 	private String title;
 
+	@NotNull(message = "Status is required")
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TaskStatus status;
+
+	@DecimalMin(value = "0.0", message = "Progress cannot be less than 0")
+	@DecimalMax(value = "100.0", message = "Progress cannot be more than 100")
+	@Column(nullable = false)
+	private float progress;
+
 	@CreatedDate
-	@Column(name = "created_at", nullable = false, updatable = false)
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
 	@LastModifiedDate
-	@Column(name = "updated_at")
+	@Column(nullable = false)
 	private LocalDateTime updatedAt;
 
 	@Column(name = "finished_at")
 	private LocalDateTime finishedAt;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "project_manager_id")
-	private ProjectManager projectManager;
+	@JoinColumn(name = "project_id", nullable = false)
+	private Project project;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "product_owner_id")
-	private ProductOwner productOwner;
+	@OneToMany(mappedBy = "backlog", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<Task> tasks = new HashSet<>();
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "team_id", nullable = false)
-	private Team team;
-
-	@OneToMany(mappedBy = "project", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private Set<Backlog> backlogs = new HashSet<>();
 }
--- src/main/java/com/app/models/Task.java
+++ src/main/java/com/app/models/Task.java
@@ -2,8 +2,13 @@ package com.app.models;
 
 import com.app.enums.TaskStatus;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,41 +23,36 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotBlank(message = "Task title is required")
-	@Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
-	@Column(nullable = false)
-	private String title;
+    @NotBlank(message = "Task title is required")
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
+    @Column(nullable = false)
+    private String title;
 
-	@NotNull(message = "Status is required")
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private TaskStatus status;
+    @NotNull(message = "Status is required")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
 
-	@DecimalMin(value = "0.0", message = "Progress cannot be less than 0")
-	@DecimalMax(value = "100.0", message = "Progress cannot be more than 100")
-	@Column(nullable = false)
-	private float progress;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-	@CreatedDate
-	@Column(nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
-	@LastModifiedDate
-	@Column(nullable = false)
-	private LocalDateTime updatedAt;
+    @Column(name = "finished_at")
+    private LocalDateTime finishedAt;
 
-	@Column(name = "finished_at")
-	private LocalDateTime finishedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private TeamMember member;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "project_id", nullable = false)
-	private Project project;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id", nullable = false)
-	private TeamMember member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "backlog_id", nullable = false)
+    private Backlog backlog;
 }
