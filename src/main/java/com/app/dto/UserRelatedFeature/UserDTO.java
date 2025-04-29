2b5aa9c719fd351aa15672f0e12b4564c9fb2e4f
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-04-29 18:27:22 +0100

    Refactor : Add the appropriate annotations to the fields

--- src/main/java/com/app/dto/UserRelatedFeature/UserDTO.java
+++ src/main/java/com/app/dto/UserRelatedFeature/UserDTO.java
@@ -1,6 +1,9 @@
-package com.app.dto;
+package com.app.dto.UserRelatedFeature;
 
 import com.app.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.io.Serializable;
@@ -8,13 +11,24 @@ import java.io.Serializable;
 /**
  * DTO for {@link User}
  */
+
 @Getter
 @Setter
+@Builder
 public class UserDTO implements Serializable {
-    Long id;
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     String email;
-    String createdAt;
-    String updatedAt;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    String password;
 }
