8528c582da5b73cb1c52b97850704830f7dcb550
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-04-29 19:10:11 +0100

    feat: Add UserService for user management operations

---
+++ src/main/java/com/app/services/UserService.java
@@ -0,0 +1,37 @@
+package com.app.services;
+
+import com.app.dto.UserRelatedFeature.UserDTO;
+
+import java.util.Optional;
+import java.util.List;
+public interface UserService {
+    /**
+     * Partially updates a user.
+     *
+     * @param userDTO the entity to update partially.
+     * @return the persisted entity.
+     */
+    Optional<UserDTO> updateUser(UserDTO userDTO, Long userId);
+
+    /**
+     * Get all the users.
+     *
+     * @return the list of entities.
+     */
+    List<UserDTO> getAllUsers();
+
+    /**
+     * Get the "id" user.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    Optional<UserDTO> getUser(Long id);
+
+    /**
+     * Delete the "id" user.
+     *
+     * @param id the id of the entity.
+     */
+    boolean delete(Long id);
+}
