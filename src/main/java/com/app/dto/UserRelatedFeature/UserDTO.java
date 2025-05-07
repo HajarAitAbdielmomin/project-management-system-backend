39aeca7bc562a50973334134c185381d99183963
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-05-07 16:05:14 +0100

    Refactor : remove jpa annotations from useDTO class

--- src/main/java/com/app/dto/UserRelatedFeature/UserDTO.java
+++ src/main/java/com/app/dto/UserRelatedFeature/UserDTO.java
@@ -1,9 +1,6 @@
 package com.app.dto.UserRelatedFeature;
 
 import com.app.models.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.io.Serializable;
@@ -18,19 +15,8 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO implements Serializable {
-    @NotBlank(message = "First name is required")
-    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     String firstName;
-
-    @NotBlank(message = "Last name is required")
-    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     String lastName;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Please provide a valid email address")
     String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters long")
     String password;
 }
