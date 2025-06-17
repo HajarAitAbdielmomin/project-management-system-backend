6155ae68f09a280bbfcf42ffdf34bda3d9730c62
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-06-17 13:11:03 +0100

    Refactor : remove additional comments

--- src/main/java/com/app/services/UserService.java
+++ src/main/java/com/app/services/UserService.java
@@ -6,33 +6,8 @@ import com.app.exceptions.UserNotFoundException;
 import java.util.Optional;
 import java.util.List;
 public interface UserService {
-    /**
-     * Partially updates a user.
-     *
-     * @param userDTO the entity to update partially.
-     * @return the persisted entity.
-     */
     Optional<UserDTO> updateUser(UserDTO userDTO, Long userId);
-
-    /**
-     * Get all the users.
-     *
-     * @return the list of entities.
-     */
     List<UserDTO> getAllUsers();
-
-    /**
-     * Get the "id" user.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
     Optional<UserDTO> getUser(Long id);
-
-    /**
-     * Delete the "id" user.
-     *
-     * @param id the id of the entity.
-     */
     boolean delete(Long id);
 }
