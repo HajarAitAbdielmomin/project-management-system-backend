476d43a501d4e905f2313c9cf27ef1ad5c28c3a7
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-07-02 02:38:42 +0100

    Add TeamsService interface with CRUD operations

---
+++ src/main/java/com/app/services/TeamsService.java
@@ -0,0 +1,23 @@
+package com.app.services;
+
+import com.app.dto.UserRelatedFeature.TeamDTO;
+import com.app.dto.UserRelatedFeature.TeamInputDto;
+import com.app.exceptions.ProjectNotFoundException;
+import com.app.exceptions.UserNotFoundException;
+import com.app.mappers.TeamInputMapper;
+import com.app.models.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamsService {
+    boolean delete(Long id);
+
+    Optional<Team> add(TeamInputDto teamInputDto) throws UserNotFoundException, ProjectNotFoundException;
+
+    Optional<Team> update(Long id, TeamInputMapper teamInputMapper);
+
+    Optional<TeamDTO> get(Long id);
+
+    Optional<List<TeamDTO>> getAll();
+}
