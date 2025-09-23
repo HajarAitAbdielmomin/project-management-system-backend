19d438fa1fa2eb5267333773c695c6da763f6bda
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-09-23 15:04:40 +0100

    Create project service interface file and the implementation project service file

--- src/main/java/com/app/services/ProjectService.java
+++ src/main/java/com/app/services/ProjectService.java
@@ -1,7 +1,13 @@
 package com.app.services;
 
+import com.app.dto.projectManagement.ProjectDTO;
+import com.app.exceptions.UserNotFoundException;
+import com.app.models.Project;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 public interface ProjectService {
+    Optional<Project> add(ProjectDTO projectDTO) throws UserNotFoundException;
 }
--- src/main/java/com/app/services/implementation/ProjectServiceImpl.java
+++ src/main/java/com/app/services/implementation/ProjectServiceImpl.java
@@ -1,8 +1,26 @@
 package com.app.services.implementation;
 
+import com.app.dto.projectManagement.ProjectDTO;
+import com.app.exceptions.UserNotFoundException;
+import com.app.mappers.ProjectMapper;
+import com.app.models.Project;
+import com.app.repository.ProjectRepository;
+import com.app.repository.UserRepository;
 import com.app.services.ProjectService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    @Override
+    public Optional<Project> add(ProjectDTO projectDTO) throws UserNotFoundException {
+        return null;
+    }
 }
