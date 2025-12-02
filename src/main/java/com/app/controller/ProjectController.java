15acbb79197f0ffbb6b911c8cabd8a4a96c50e67
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-02 18:09:24 +0100

    feat: Use the implemented API to create a project

--- src/main/java/com/app/controller/ProjectController.java
+++ src/main/java/com/app/controller/ProjectController.java
@@ -1,5 +1,26 @@
 package com.app.controller;
 
-public class ProjectController {
+import com.app.dto.projectManagement.ProjectDTO;
+import com.app.exceptions.UserNotFoundException;
+import com.app.models.Project;
+import com.app.services.implementation.ProjectServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectServiceImpl projectService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addProject(@Valid @RequestBody ProjectDTO projectDTO)
+            throws UserNotFoundException {
+        return projectService.add(projectDTO).isPresent() ?
+                ResponseEntity.ok("Project added successfully") :
+                ResponseEntity.badRequest().body("Project not added");
+    }
 }
