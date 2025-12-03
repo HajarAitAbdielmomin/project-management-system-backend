8f8c21e42dbdc2dde9bd6c9e1204123693957734
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-03 20:46:16 +0100

    feat: add operations for project management handling

--- src/main/java/com/app/services/ProjectService.java
+++ src/main/java/com/app/services/ProjectService.java
@@ -1,13 +1,19 @@
 package com.app.services;
 
 import com.app.dto.projectManagement.ProjectDTO;
+import com.app.dto.projectManagement.ProjectDetailsDTO;
+import com.app.exceptions.ProjectNotFoundException;
 import com.app.exceptions.UserNotFoundException;
 import com.app.models.Project;
-import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ProjectService {
     Optional<Project> add(ProjectDTO projectDTO) throws UserNotFoundException;
+    Optional<ProjectDetailsDTO> getProjectDetails(Long id) throws ProjectNotFoundException;
+    boolean delete(Long id) throws ProjectNotFoundException;
+    Optional<List<ProjectDetailsDTO>> getAllProjectsByUser(Long id);
+    Optional<Project> update(Long id, ProjectDTO projectDTO);
 }
