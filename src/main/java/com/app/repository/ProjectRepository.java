98a5df2b8ff4f26693734bf25df6d91c9bb5c067
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-04 17:18:08 +0100

    Add findAllByUerId, a query method to retrieve projects by user ID

--- src/main/java/com/app/repository/ProjectRepository.java
+++ src/main/java/com/app/repository/ProjectRepository.java
@@ -5,7 +5,10 @@ import com.app.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-
+    Optional<List<Project>> findAllByUserId(Long userId);
 }
