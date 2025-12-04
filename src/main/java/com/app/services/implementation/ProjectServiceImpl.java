abdad78f39a5fa96ff5416714bede5c1661821de
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-04 18:04:05 +0100

    Add update method with title and description modification. - Implement delete method to use Optional pattern without exceptions. - Implement getProjectDetails with ProjectNotFoundException handling. - Implement getAllProjectsByUser with DTO mapping using projectDetailsMapper to retrieve the projects created by the PM with the id passed in param.

--- src/main/java/com/app/services/implementation/ProjectServiceImpl.java
+++ src/main/java/com/app/services/implementation/ProjectServiceImpl.java
@@ -1,28 +1,37 @@
 package com.app.services.implementation;
 
 import com.app.dto.projectManagement.ProjectDTO;
+import com.app.dto.projectManagement.ProjectDetailsDTO;
+import com.app.exceptions.ProjectNotFoundException;
 import com.app.exceptions.UserNotFoundException;
+import com.app.mappers.ProjectDetailsMapper;
 import com.app.mappers.ProjectMapper;
 import com.app.models.ProductOwner;
 import com.app.models.Project;
 import com.app.models.ProjectManager;
+import com.app.models.Team;
 import com.app.repository.ProjectManagerRepository;
 import com.app.repository.ProductOwnerRepository;
 import com.app.repository.ProjectRepository;
-import com.app.repository.UserRepository;
+import com.app.repository.TeamRepository;
 import com.app.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
     private final ProjectManagerRepository projectManagerRepository;
     private final ProductOwnerRepository projectOwnerRepository;
+    private final ProjectDetailsMapper projectDetailsMapper;
+    private  final TeamRepository teamRepository;
 
     @Override
     public Optional<Project> add(ProjectDTO projectDTO) throws UserNotFoundException {
@@ -46,4 +55,37 @@ public class ProjectServiceImpl implements ProjectService {
 
         return Optional.of(savedProject);
     }
+    @Override
+    public Optional<Project> update(Long id, ProjectDTO projectDTO) {
+        Optional<Project> project = projectRepository.findById(id);
+        if (project.isPresent()) {
+            Project projectToUpdate = project.get();
+            projectToUpdate.setTitle(projectDTO.getTitle());
+            projectToUpdate.setDescription(projectDTO.getDescription());
+            return Optional.of(projectRepository.save(projectToUpdate));
+        } else return Optional.empty();
+
+    }
+    @Override
+    public boolean delete(Long id) {
+        Optional<Project> project = projectRepository.findById(id);
+        project.ifPresent(projectRepository::delete);
+        return project.isPresent();
+    }
+    @Override
+    public Optional<ProjectDetailsDTO> getProjectDetails(Long id) throws ProjectNotFoundException{
+        Project project = projectRepository.findById(id).orElseThrow(
+                () -> new ProjectNotFoundException("Project not found with the id "+id)
+        );
+        return Optional.of(projectDetailsMapper.toDto(project));
+    }
+    @Override
+    public Optional<List<ProjectDetailsDTO>> getAllProjectsByUser(Long id) {
+        Optional<List<Project>> projects = projectRepository.findAllByUserId(id);
+        return projects.map(projectList
+                -> projectList.stream().map(projectDetailsMapper::toDto).toList());
+    }
+
+
+
 }
