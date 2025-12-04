79218df05ea0db48dd93f8005ad87337b0a82653
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-04 17:07:06 +0100

    Add Mapstruct interface for the ProjectDetailsDTO

--- src/main/java/com/app/mappers/ProjectDetailsMapper.java
+++ src/main/java/com/app/mappers/ProjectDetailsMapper.java
@@ -1,4 +1,15 @@
 package com.app.mappers;
 
+import com.app.dto.projectManagement.ProjectDetailsDTO;
+import com.app.models.Project;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProjectDetailsMapper {
+    Project toEntity(ProjectDetailsDTO projectDetailsDTO);
+
+    ProjectDetailsDTO toDto(Project project);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Project partialUpdate(ProjectDetailsDTO projectDetailsDTO, @MappingTarget Project project);
 }
