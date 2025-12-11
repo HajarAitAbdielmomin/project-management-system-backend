19cd5bc75cd85183c204f06e73433b0df2f64dc0
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-11 20:09:25 +0100

    feat: add full CRUD operations to the project controller

--- src/main/java/com/app/controller/ProjectController.java
+++ src/main/java/com/app/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.app.controller;
 
 import com.app.dto.projectManagement.ProjectDTO;
+import com.app.exceptions.ProjectNotFoundException;
 import com.app.exceptions.UserNotFoundException;
 import com.app.models.Project;
 import com.app.services.implementation.ProjectServiceImpl;
@@ -22,5 +23,30 @@ public class ProjectController {
         return projectService.add(projectDTO).isPresent() ?
                 ResponseEntity.ok("Project added successfully") :
                 ResponseEntity.badRequest().body("Project not added");
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getProject(@PathVariable Long id) throws ProjectNotFoundException {
+        return ResponseEntity.ok(projectService.getProjectDetails(id));
+
+    }
+
+    @GetMapping("/getAll/{id}")
+    public ResponseEntity<?> getAllProjects(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.getAllProjectsByUser(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProject(@PathVariable Long id) {
+        return projectService.delete(id) ?
+                ResponseEntity.ok("Project deleted successfully") :
+                ResponseEntity.badRequest().body("Project not found");
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> updateProject(@PathVariable Long id, @Valid @RequestBody ProjectDTO projectDTO) {
+        return projectService.update(id, projectDTO).isPresent() ?
+                ResponseEntity.ok("Project updated successfully") :
+                ResponseEntity.badRequest().body("Project not updated");
     }
 }
