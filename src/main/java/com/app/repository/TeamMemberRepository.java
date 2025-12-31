1ac8cc907bac9da1f7b0784824b22d13c0d52778
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-31 13:04:16 +0100

    Add findTeamMembersByTeam query method to TeamMemberRepository

--- src/main/java/com/app/repository/TeamMemberRepository.java
+++ src/main/java/com/app/repository/TeamMemberRepository.java
@@ -1,12 +1,14 @@
 package com.app.repository;
 
+import com.app.models.Team;
 import com.app.models.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long>{
-
+    Optional<List<TeamMember>> findTeamMembersByTeam(Team team);
 }
