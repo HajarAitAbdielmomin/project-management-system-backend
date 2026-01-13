261af7f99eef1c1c48b8b8bc3c28650edb9aff49
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-13 15:58:19 +0100

    implement the apis for updating and getting the team

--- src/main/java/com/app/controller/TeamController.java
+++ src/main/java/com/app/controller/TeamController.java
@@ -24,4 +24,20 @@ public class TeamController {
                 ResponseEntity.ok("Team created successfully") :
                 ResponseEntity.badRequest().body("Team not created");
     }
+    @PatchMapping("/update/{id}")
+    //@PreAuthorize("hasRole('project manager')")
+   public ResponseEntity<?> updateTeam(@PathVariable Long id, @Valid @RequestBody TeamDTO teamDTO)
+            throws ProjectNotFoundException, UserNotFoundException, com.app.exceptions.TeamNotFoundException
+    {
+        return teamsService.update(id, teamDTO) ?
+                ResponseEntity.ok("Team updated successfully") :
+                ResponseEntity.badRequest().body("Team not updated");
+    }
+
+    @GetMapping("/get/{id}")
+    //@PreAuthorize("hasRole('project manager')")
+    public ResponseEntity<?> getTeam(@PathVariable Long id) {
+        return teamsService.get(id).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
