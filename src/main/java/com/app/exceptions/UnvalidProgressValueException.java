4de35896631a8a55d222fce05c715de035c6d8fe
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-19 15:21:45 +0100

    create execption to manage the progress value

---
+++ src/main/java/com/app/exceptions/UnvalidProgressValueException.java
@@ -0,0 +1,7 @@
+package com.app.exceptions;
+
+public class UnvalidProgressValueException extends RuntimeException {
+    public UnvalidProgressValueException(String message) {
+        super(message);
+    }
+}
