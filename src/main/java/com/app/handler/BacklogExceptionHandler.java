edd85857f8886fdf061e2fe6fb5942e76671c808
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-19 15:35:27 +0100

    create the backlog exception handler, place the the progress value handler within it and remove the progress value handler file

--- src/main/java/com/app/handler/BacklogExceptionHandler.java
+++ src/main/java/com/app/handler/BacklogExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.app.handler;
 
+import com.app.exceptions.BacklogNotFoundException;
+import com.app.exceptions.UnvalidProgressValueException;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class BacklogExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<String> handleBacklogIdException(BacklogNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleProgressNotFoundException(UnvalidProgressValueException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
