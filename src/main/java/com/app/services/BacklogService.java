cb17fe180b9480334c844b74b305d40640e18839
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-19 17:52:26 +0100

    add crud operations methods prototypes

--- src/main/java/com/app/services/BacklogService.java
+++ src/main/java/com/app/services/BacklogService.java
@@ -1,4 +1,19 @@
 package com.app.services;
 
+import com.app.dto.projectManagement.BacklogDTO;
+import com.app.dto.projectManagement.BacklogDetailsDTO;
+import com.app.exceptions.BacklogNotFoundException;
+import com.app.exceptions.ProjectNotFoundException;
+import com.app.exceptions.UnvalidProgressValueException;
+import com.app.models.Backlog;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BacklogService {
+    boolean add(BacklogDTO backlogDTO) throws ProjectNotFoundException;
+    boolean delete(Long id);
+    boolean update(Long id, BacklogDTO backlogDTO) throws BacklogNotFoundException, ProjectNotFoundException, UnvalidProgressValueException;
+    Optional<BacklogDTO> getBacklog(Long id);
+    List<BacklogDetailsDTO> getAllBacklogsByProject(Long projectId);
 }
