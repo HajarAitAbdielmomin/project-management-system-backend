af0133f0a8665a227fe52a16696929a5db3f2bab
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-19 15:54:30 +0100

    define the method findBacklogsByProject in the Backlog Repo

--- src/main/java/com/app/repository/BacklogRepository.java
+++ src/main/java/com/app/repository/BacklogRepository.java
@@ -1,9 +1,13 @@
 package com.app.repository;
 
 import com.app.models.Backlog;
+import com.app.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BacklogRepository extends JpaRepository<Backlog, Long> {
+    List<Backlog> findBacklogsByProject(Project project);
 }
