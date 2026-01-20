59ddb6ffbd55b4788ef8d7166a58622db7d58d08
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-20 11:54:38 +0100

    add exceptions to the prototypes of add and update methods

--- src/main/java/com/app/services/BacklogService.java
+++ src/main/java/com/app/services/BacklogService.java
@@ -2,6 +2,7 @@ package com.app.services;
 
 import com.app.dto.projectManagement.BacklogDTO;
 import com.app.dto.projectManagement.BacklogDetailsDTO;
+import com.app.exceptions.BacklogAlreadyExistsException;
 import com.app.exceptions.BacklogNotFoundException;
 import com.app.exceptions.ProjectNotFoundException;
 import com.app.exceptions.UnvalidProgressValueException;
@@ -11,9 +12,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BacklogService {
-    boolean add(BacklogDTO backlogDTO) throws ProjectNotFoundException;
+    boolean add(BacklogDTO backlogDTO) throws ProjectNotFoundException, BacklogAlreadyExistsException;
     boolean delete(Long id);
-    boolean update(Long id, BacklogDTO backlogDTO) throws BacklogNotFoundException, ProjectNotFoundException, UnvalidProgressValueException;
+    boolean update(Long id, BacklogDTO backlogDTO) throws  BacklogAlreadyExistsException, BacklogNotFoundException, ProjectNotFoundException, UnvalidProgressValueException;
     Optional<BacklogDTO> getBacklog(Long id);
     List<BacklogDetailsDTO> getAllBacklogsByProject(Long projectId);
 }
