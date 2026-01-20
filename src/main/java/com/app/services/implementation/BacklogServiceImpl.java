1ad1d11b4bc1a2b7f7192cf56e2569399523845b
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-20 14:52:16 +0100

    implement the api within the backlog service for adding new backlog

--- src/main/java/com/app/services/implementation/BacklogServiceImpl.java
+++ src/main/java/com/app/services/implementation/BacklogServiceImpl.java
@@ -2,10 +2,13 @@ package com.app.services.implementation;
 
 import com.app.dto.projectManagement.BacklogDTO;
 import com.app.dto.projectManagement.BacklogDetailsDTO;
+import com.app.exceptions.BacklogAlreadyExistsException;
 import com.app.exceptions.BacklogNotFoundException;
 import com.app.exceptions.ProjectNotFoundException;
 import com.app.exceptions.UnvalidProgressValueException;
 import com.app.mappers.BacklogMapper;
+import com.app.models.Backlog;
+import com.app.models.Project;
 import com.app.repository.BacklogRepository;
 import com.app.repository.ProjectRepository;
 import com.app.services.BacklogService;
@@ -19,8 +22,22 @@ public class BacklogServiceImpl implements BacklogService {
     BacklogMapper backlogMapper;
 
     @Override
-    public boolean add(BacklogDTO backlogDTO) throws ProjectNotFoundException{
-        return false;
+    public boolean add(BacklogDTO backlogDTO) throws ProjectNotFoundException, BacklogAlreadyExistsException {
+        Backlog backlog = backlogMapper.toEntity(backlogDTO);
+
+        if(backlog == null) return false;
+
+        if(backlogRepository.existsBacklogByTitle(backlog.getTitle()))
+            throw new BacklogAlreadyExistsException(String.format("Backlog with title %s already exists", backlog.getTitle()));
+
+        Project project = projectRepository.findById(backlogDTO.getProjectId()).orElseThrow(
+                () -> new ProjectNotFoundException(String.format("Project with id %d not found", backlogDTO.getProjectId())));
+
+        backlog.setProject(project);
+
+        backlogRepository.save(backlog);
+
+        return true;
     }
 
     @Override
