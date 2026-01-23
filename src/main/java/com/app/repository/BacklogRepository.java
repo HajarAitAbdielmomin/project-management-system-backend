44518f5d151f0893eddeaad07ab4c210d516df17
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-23 18:34:55 +0100

    implement the methods for requesting backlogs information and integrate apis implemented in the service layer

--- src/main/java/com/app/repository/BacklogRepository.java
+++ src/main/java/com/app/repository/BacklogRepository.java
@@ -3,6 +3,8 @@ package com.app.repository;
 import com.app.models.Backlog;
 import com.app.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,5 @@ import java.util.List;
 public interface BacklogRepository extends JpaRepository<Backlog, Long> {
     List<Backlog> findBacklogsByProjectId(Long id);
     boolean existsBacklogByProject_IdAndTitle(Long projectId, String title);
-    boolean existsBacklogByTitleAndIdNot(String title, Long id);
+    boolean existsBacklogByProject_IdAndTitleAndIdNot(Long projectId, String title, Long id);
 }
