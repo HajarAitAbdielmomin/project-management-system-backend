3303e4df4881e5b634781ddab64e3b5a72c7ff41
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-26 16:09:29 +0100

    create the task controller for task APIs management

---
+++ src/main/java/com/app/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class TaskController {
+}
