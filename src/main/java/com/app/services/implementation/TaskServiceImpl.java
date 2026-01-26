e49c6521953be337bea33f7cfb9babb24658dc15
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-26 16:06:14 +0100

    add @Service, @RequiredArgsConstructor and @Transactional annotations to the implementation class of TaskService

--- src/main/java/com/app/services/implementation/TaskServiceImpl.java
+++ src/main/java/com/app/services/implementation/TaskServiceImpl.java
@@ -1,4 +1,11 @@
 package com.app.services.implementation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class TaskServiceImpl {
 }
