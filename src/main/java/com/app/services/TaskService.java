34a0b0e2ae6b21f5cb1592bc23a51a79d645b495
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-01-27 16:24:49 +0100

    define the methods for api implementation of tasks management

--- src/main/java/com/app/services/TaskService.java
+++ src/main/java/com/app/services/TaskService.java
@@ -1,4 +1,21 @@
 package com.app.services;
 
+import com.app.dto.TaskManagement.TaskDTO;
+import com.app.dto.TaskManagement.TaskDetailsDTO;
+import com.app.exceptions.BacklogNotFoundException;
+import com.app.exceptions.TaskAlreadyExistsException;
+import com.app.exceptions.TaskNotFoundException;
+import com.app.exceptions.UserNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskService {
+    boolean add(TaskDTO taskDTO) throws BacklogNotFoundException, TaskAlreadyExistsException, UserNotFoundException;
+    boolean delete(Long id);
+    boolean update(Long id, TaskDTO taskDTO) throws TaskNotFoundException, UserNotFoundException;
+    Optional<TaskDetailsDTO> getTask(Long id);
+    Long getTasksCountByBacklogId(Long backlogId);
+    Long getTasksCountByMemberId(Long memberId);
+    List<TaskDetailsDTO> getTasksByBacklog(Long backlogId);
 }
