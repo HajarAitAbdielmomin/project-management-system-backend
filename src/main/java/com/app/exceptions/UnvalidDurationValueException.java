0ef79832ffe8ea3454d854bb980a88d82ecbf524
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-02-03 16:11:24 +0100

    create UnvalidDurationValue Exception

--- src/main/java/com/app/exceptions/UnvalidDurationValueException.java
+++ src/main/java/com/app/exceptions/UnvalidDurationValueException.java
@@ -1,7 +1,7 @@
 package com.app.exceptions;
 
 public class UnvalidDurationValueException extends RuntimeException {
-  public UnvalidDurationValueException(String message) {
-    super(message);
-  }
+    public UnvalidDurationValueException(String message) {
+        super(message);
+    }
 }
